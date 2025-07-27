5dd402cac511116d04f68b27619eb5713487511b
Ninra <jorgejjvm17@gmail.com>
2025-07-27 11:54:22 -0400

    Update languageSettings.js

--- languageSettings.js
+++ languageSettings.js
@@ -15,7 +15,8 @@ document.addEventListener('DOMContentLoaded', () =>
     {
         aboutmeText01.textContent = `About me`
         
-        aboutmeText02.textContent = `I am an independent developer taking my first steps in the Unity Asset Store. I specialize in creating add-ons for the MMORPG Kit, as I am also working on my own MMORPG based on this asset.
+        aboutmeText02.textContent = `I am an independent developer taking my first steps in the Unity Asset Store. 
+        I specialize in creating add-ons for the MMORPG Kit, as I am also working on my own MMORPG based on this asset.
         Most of my assets are used directly in my project "Gaiden Online", which allows me to guarantee their proper functioning and ensure that they are attractive and truly useful.
         I am confident in the quality of my creations and hope you will give them a try. Thank you in advance for your support!`.trim();
         
